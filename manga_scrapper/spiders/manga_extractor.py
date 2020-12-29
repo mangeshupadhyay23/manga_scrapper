de59c9950fbca08a235846b5d378367785d09766
mangesh <mshanit@gmail.com>
2020-12-30 01:35:11 +0530

    added function for extracting src file for pages

--- manga_scrapper/spiders/manga_extractor.py
+++ manga_scrapper/spiders/manga_extractor.py
@@ -1,12 +1,14 @@
 import scrapy
-
+actual_url= ""
 
 class MangaExtractorSpider(scrapy.Spider):
+    
     name = 'manga_extractor'
     allowed_domains = ['www.mangareader.net']
     start_urls = ['https://www.mangareader.net/alphabetical']
 
     def parse(self, response):
+
         titles = response.xpath("//div[@class='d40']/div/ul/li")
         
         for mangas in titles:
@@ -14,6 +16,7 @@ class MangaExtractorSpider(scrapy.Spider):
             manga_url = mangas.xpath(".//a/@href").get()
             
             yield response.follow(url = manga_url, callback = self.parse_manga, meta={'title' : title})
+            
 
     def parse_manga(self,response):
         title = response.request.meta['title']
@@ -21,25 +24,32 @@ class MangaExtractorSpider(scrapy.Spider):
 
         for chapter in chapters:
             chapter_count = chapter.xpath(".//td[1]/a/text()").get()
-            chapter_name = chapter.xpath(".//td[1]/text()[2]").get()
+            #chapter_name = chapter.xpath(".//td[1]/text()[2]").get()
             chapter_url = chapter.xpath(".//td[1]/a/@href").get()
-            date_uploaded = chapter.xpath(".//td[2]/text()").get()
+            #date_uploaded = chapter.xpath(".//td[2]/text()").get()
 
-            yield{
-                'title':title,
-                'chapter':f"{chapter_count} {chapter_name}",
-                'date_Added':date_uploaded
-            }
+            # yield{
+            #     'title':title,
+            #     'chapter':f"{chapter_count} {chapter_name}",
+            #     'date_Added':date_uploaded
+            # }
 
-            yield response.follow(url = chapter_url, callback = self.parse_manga_chapter,meta={'page_no':1, 'actual_url': chapter_url} )
+            global actual_url
+            actual_url = response.urljoin(chapter_url)
+            
+            yield response.follow(url = chapter_url, callback = self.parse_manga_chapter,meta={'page_no':1, 'title':title, 'chapter_count':chapter_count} )
 
             
     
     def parse_manga_chapter(self,response):
         page_src = response.xpath("//img[@id='ci']/@src").get()
-        actual_url = response.url
         page_no = response.request.meta['page_no']
+        title = response.request.meta['title']
+        chapter_count = response.request.meta['chapter_count']
         yield{
+            "title":title,
+            "chapter_count":chapter_count,
+            "page_no":page_no,
             "image_src":page_src
         }
         
@@ -47,8 +57,7 @@ class MangaExtractorSpider(scrapy.Spider):
         
         next_page_url = f"{actual_url}/{page_no}"
         print(next_page_url)
-        while page_no < 5:
-            yield scrapy.Request(next_page_url,callback=self.parse_manga_chapter,meta={'page_no':page_no, 'actual_url': actual_url})
+        yield scrapy.Request(url= next_page_url,callback=self.parse_manga_chapter,meta={'page_no':page_no, 'title':title, 'chapter_count':chapter_count})
 
 
         
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Web scraper made using scrapy for web-scraping  mangareader.com
+
+It is a tool made using scrapy (python based web scraping framework) for scraping the data all the available titles , their chapters and each page of each chapter.
+
+## Usage
+
+```sh
+$ pip install scrapy
+$ scrapy crawl manga_extractor -o <dataset_name.json or dataset_name.csv>
+```
